42fc073db1861a52a7c1eb2ab67f9c5958d0f328
chaymaeamouri <chaymaeamouri68@gmail.com>
2026-07-04 02:04:45 +0200

    chore: remove temp files

---
+++ cleanup.bat
@@ -0,0 +1,4 @@
+del push.bat
+git add .
+git commit -m "chore: remove temp files"
+git push origin main
--- push.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "clean: suppression commentaires et mise a jour README"
-git push origin main
